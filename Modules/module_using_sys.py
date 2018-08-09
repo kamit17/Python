d52cfe7caa343b8d1436a5c41526738b753906e1
kamit17 <kamit17@outlook.com>
2018-08-08 21:50:48 -0500

    Working on classes and objects

---
+++ Modules/module_using_sys.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import sys
+print('The command line argumenrs are:')
+for i in sys.argv:
+    print(i)
+    
+print('\n\nThe PYTHONPATH is',sys.path, '\n')
---
+++ Random_Programs/str_format.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  4 11:55:38 2018
+
+@author: Maverick
+"""
+
+age = 30
+name = 'Maverick'
+
+print('{0} was {1} years old when he wrote this book'.format(name, age))
+print('Why is {0} playing with that python?'.format(name))
