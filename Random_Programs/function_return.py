c4257f891b6491a866076fb1dec94183560eea85
kamit17 <kamit17@outlook.com>
2018-08-14 21:20:17 -0500

    updates

---
+++ Random_Programs/function_return.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  8 22:16:02 2018
+
+@author: Maverick
+"""
+
+def maximum(x,y):
+    if x > y:
+        #return x
+    elif x == y:
+        return 'The numbers are equal'
+    else:
+        return y
+    
+print(maximum(2,3))
