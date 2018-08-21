847094426a34283c1a2c167e0e2d0e348fdb99d9
kamit17 <kamit17@outlook.com>
2018-08-21 14:17:31 -0500

    Working on Practice Python org exercises

---
+++ Practice Python/Character_input.py
@@ -0,0 +1,16 @@
+#Program to ask user for their name and age and print out when they will be 100
+
+name = input('Give me your name: ')
+#print('Your name is : ' + name)
+
+age = int(input('Enter your age: '))
+#print('your age is : '+ str(age))
+
+year = str((2017 -age) + 100)
+
+print(year)
+
+
+
+
+    
