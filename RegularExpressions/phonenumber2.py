44603c8fafdada9cc2662796533c2b1d6c82bc68
kamit17 <kamit17@outlook.com>
2018-08-29 20:13:59 -0500

    New

--- RegularExpressions/phonenumber2.py
+++ RegularExpressions/phonenumber2.py
@@ -9,6 +9,6 @@ Created on Thu Aug 23 21:02:46 2018
 import re
 message = 'Call me at 415-555-1011 tomorrow. 415-555-9999 is my office.'
 
-phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
-mo = phoneNumRegex.search(message)
-print(mo.group())
+phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')  #call re.compile ()function to create a regex object.
+mo = phoneNumRegex.search(message) #the search method searches the string for any matches to regex.If match is foud, returns a Match object.
+print(mo.group()) #match objects have a group() mmethod that returns the actual mathced textfrom string .
