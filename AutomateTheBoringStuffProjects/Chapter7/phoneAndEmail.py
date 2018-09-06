7c39c20dc1f87e231e6adf47d4af316fa7e6a166
kamit17 <kamit17@outlook.com>
2018-09-06 15:22:52 -0700

    New

---
+++ AutomateTheBoringStuffProjects/Chapter7/phoneAndEmail.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  6 15:08:23 2018
+
+@author: IEUser
+"""
+
+#! python3
+
+import re, pyperclip
+#TODOL Create a regex for phone numbers
+
+#TODO: create a regex for email addressses
+
+#TOTO: Get the text off the clipboard
+
+#TODO: Extract the email/phone from this text
+
+#TODO: Copy the extracted email/phone to the clipboard
