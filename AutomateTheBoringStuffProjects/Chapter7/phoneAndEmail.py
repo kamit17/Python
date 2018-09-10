b8b2cae8b140a861e8643e8c942edd181adab5b7
kamit17 <kamit17@outlook.com>
2018-09-10 12:24:20 -0700

    Unable to understand the process

--- AutomateTheBoringStuffProjects/Chapter7/phoneAndEmail.py
+++ AutomateTheBoringStuffProjects/Chapter7/phoneAndEmail.py
@@ -6,9 +6,19 @@ Created on Thu Sep  6 15:08:23 2018
 """
 
 #! python3
+#phoneAndEmail.py - Finds phone numbers and email addresses on the clipboard
 
 import re, pyperclip
-#TODOL Create a regex for phone numbers
+#TODO: Create a regex for phone numbers
+
+phoneRegex = re.compile(r'''(
+			\d{3}|\(\d{3}\))?                             #area code
+			(\s|-|\.)?                                    #Seperator
+			(\d{3})                                       #first 3 digits
+			(\s|-|\.)                                     #seperator
+			(\d{4})									      #last 4 digits
+			(\s*(ext|x|ext.)\s*(\d{2,5}))?                #extension
+			)''',re.VERBOSE)
 
 #TODO: create a regex for email addressses
 
