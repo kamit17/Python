249d75032465153c9fde603e7d7feff328ba0f84
kamit17 <kamit17@outlook.com>
2018-09-18 13:58:59 -0700

    Still working

--- AutomateTheBoringStuffProjects/Chapter7/phoneAndEmail.py
+++ AutomateTheBoringStuffProjects/Chapter7/phoneAndEmail.py
@@ -13,22 +13,38 @@ import re, pyperclip
 
 phoneRegex = re.compile(r'''(
 			\d{3}|\(\d{3}\))?                             #area code
-			(\s|-|\.)?                                    #Seperator
-			(\d{3})                                       #first 3 digits
-			(\s|-|\.)                                     #seperator
-			(\d{4})									        #last 4 digits
-			(\s*(ext|x|ext.)\s*(\d{2,5}))?                #extension
+			(\s|-|\.)?                                    
+			(\d{3})                                       
+			(\s|-|\.)                                    
+			(\d{4})									        
+			(\s*(ext|x|ext.)\s*(\d{2,5}))?                
 			)''',re.VERBOSE)
 
-#TODO: create a regex for email addressses
+#: create a regex for email addressses
 emailRegex = re.compile(r'''(
         [a-zA-Z0-9._%+-]+
         @
-        [a-zA-Z0-9.])
-                        )
+        [a-zA-Z0-9.-]+(\.a-zA-Z]{2,4}))''',
+		re.VERBOSE)
 
-#TOTO: Get the text off the clipboard
+#Find matches in clipboard text
 
-#TODO: Extract the email/phone from this text
+text = str(pyperclip.paste())
+matches = []
+for groups in phoneRegex.findall(text):
+	phoneNum = '-'.join([groups[1],groups[3],groups[5]])
+	if groups[8] !='':
+		phoneNum += ' x' + groups[8]
+	matches.append(phoneNum)
+for groups in emailRegex.findall(text):
+	matches.append(groups[0])
+
+#Copy  Results to the clipboard
+if len(matches) > 0:
+	pyperclip.copy('\n'.join(matches))
+	print('Copied to clipboard:')
+	print('\n'.join(matches))
+else:
+	print('No phone numbers or email addresses found.')
 
 #TODO: Copy the extracted email/phone to the clipboard
