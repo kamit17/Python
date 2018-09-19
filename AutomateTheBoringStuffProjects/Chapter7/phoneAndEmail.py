4f4734bee2ff874008d25e7d2a477e3bb911a652
kamit17 <kamit17@outlook.com>
2018-09-19 11:40:25 -0700

    Initial Commit for Project

--- AutomateTheBoringStuffProjects/Chapter7/phoneAndEmail.py
+++ AutomateTheBoringStuffProjects/Chapter7/phoneAndEmail.py
@@ -6,31 +6,32 @@ Created on Thu Sep  6 15:08:23 2018
 """
 
 #! python3
-#phoneAndEmail.py - Finds phone numbers and email addresses on the clipboard
+# phoneAndEmail.py - Finds phone numbers and email addresses on the clipboard
 
 import re, pyperclip
 #TODO: Create a regex for phone numbers
 
 phoneRegex = re.compile(r'''(
-			\d{3}|\(\d{3}\))?                             #area code
-			(\s|-|\.)?                                    
-			(\d{3})                                       
-			(\s|-|\.)                                    
-			(\d{4})									        
-			(\s*(ext|x|ext.)\s*(\d{2,5}))?                
-			)''',re.VERBOSE)
+    (\d{3}|\(\d{3}\))?                # area code
+    (\s|-|\.)?                        # separator
+    (\d{3})                           # first 3 digits
+    (\s|-|\.)                         # separator
+    (\d{4})                           # last 4 digits
+    (\s*(ext|x|ext.)\s*(\d{2,5}))?    # extension
+    )''', re.VERBOSE)
 
-#: create a regex for email addressses
+# Create email regex.
 emailRegex = re.compile(r'''(
-        [a-zA-Z0-9._%+-]+
-        @
-        [a-zA-Z0-9.-]+(\.a-zA-Z]{2,4}))''',
-		re.VERBOSE)
+    [a-zA-Z0-9._%+-]+      # username
+    @                      # @ symbol
+    [a-zA-Z0-9.-]+         # domain name
+    (\.[a-zA-Z]{2,4})      # dot-something
+    )''', re.VERBOSE)
 
 #Find matches in clipboard text
 
 text = str(pyperclip.paste())
-matches = []
+matches = []                        #Store matches
 for groups in phoneRegex.findall(text):
 	phoneNum = '-'.join([groups[1],groups[3],groups[5]])
 	if groups[8] !='':
@@ -47,4 +48,4 @@ if len(matches) > 0:
 else:
 	print('No phone numbers or email addresses found.')
 
-#TODO: Copy the extracted email/phone to the clipboard
+
