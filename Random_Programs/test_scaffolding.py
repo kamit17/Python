30d5269de4723168e15eccced42282efa022612e
Amit Kumar <kamit17@outlook.com>
2019-07-29 22:33:31 -0500

    Python3

---
+++ Random_Programs/test_scaffolding.py
@@ -0,0 +1,14 @@
+'''
+put your test here:
+    >>> type(thing1)
+    <class 'list'>
+    >>>type(thing2)
+    <class 'tuple'>
+    >>> type(thing3)
+    <class 'str'>
+'''
+#put your solutions here:
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
