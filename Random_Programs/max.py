05a8f5e0fa3b9369dceb1dbc18f5cd93644d82be
Amit Chauhan <kamit17@outlook.com>
2019-08-02 14:15:03 +0000

    new

---
+++ Random_Programs/max.py
@@ -0,0 +1,6 @@
+def max( x,  y):
+    if (x > y):
+        print('x is greater')
+    else:
+        print('y is greater')
+max(7,8)
---
+++ Random_Programs/max3.py
@@ -0,0 +1,9 @@
+def max_of_three(x,y,z):
+    if (x > y) and (x > z):
+        print('x is greater')
+    elif (y>z) and (y>x):
+        print('Y is greater')
+    else:
+        print('z is greater')
+max_of_three(12,22,33)
+            
