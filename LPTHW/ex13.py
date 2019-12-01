bc888d8573b1a8803d97de72cbe4dccb50336fcc
kamit17 <kamit17@outlook.com>
2019-11-30 20:38:43 -0600

    LPTHW

---
+++ LPTHW/ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script, first,second,third = argv
+
+print('The script is called:',script)
+print('The first variable is:',first)
+print('the second variable is:',second)
+print('The third variable is:',third)
---
+++ LPTHW/ex32.py
@@ -0,0 +1,31 @@
+the_count = [1,2,3,4,5]
+fruits = ['apples','oranges','pears','apricots']
+change = [1,'pennies',2,'dimes',3,'quarters']
+
+#for loop to go through a list
+for number in the_count:
+    print(f"This is count {number}")
+
+
+#same as above
+for fruit in fruits:
+    print(f'A fruit of type: {fruit}')
+
+#going through mixed list. using {} since we do not know what is in it
+for i in change:
+    print(f'I got {i}')
+
+#we can also build lists,
+#empty list
+elements = []
+
+#then use the range function to do 0 to 5 counts
+
+for i in range(0,6):
+    print(f'Adding {i} to the list.')
+    #append to the list
+    elements.append(i)
+
+#print out the list
+for i in elements:
+    print(f'Element was: {i}')
