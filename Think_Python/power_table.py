eac2eae883e2f3220f80363a8c3186146d8d9e1b
kamit17 <kamit17@outlook.com>
2020-04-08 03:58:47 -0500

    Think Python

---
+++ Think_Python/power_table.py
@@ -0,0 +1,2 @@
+for x in range(13): # Generate numbers 0 to 12
+    print(x, "\t", 2 ** x)
---
+++ Think_Python/prime.py
@@ -0,0 +1,9 @@
+x = int(input('Enter the value of x: '))
+if x % 2== 0:
+    print(x, " is even.")
+    print("Did you know that 2 is the only even number that is prime?")
+    
+else:
+    print(x, " is odd.")
+    print("Did you know that multplying two odd numbers " +
+          " always gives an odd result?")
