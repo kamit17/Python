4cf1c48b2fb7839356e2ef72d1e2b3d2861af153
kamit17 <kamit17@outlook.com>
2020-04-29 16:50:19 -0400

    W3Resource

--- W3Resource Exercises/Strings/ex4.py
+++ W3Resource Exercises/Strings/ex4.py
@@ -4,3 +4,10 @@ of its first char have been changed to '$', except the first char itself.
 Sample String : 'restart'
 Expected Result : 'resta$t'
 """
+
+string1 = input('Enter the string : ')
+char = string1[0]
+print(string1[1:])
+string1 = string1.replace(char, '$')
+string1 = char + string1[1:]
+print(string1)
