ec81460e3b6670c994d61d48b8867c7a3960954e
kamit17 <kamit17@outlook.com>
2020-07-14 14:14:53 -0500

    W3Resource

--- W3Resource_Exercises/Strings/ex12.py
+++ W3Resource_Exercises/Strings/ex12.py
@@ -1,3 +1,15 @@
 #12. Write a Python program to count the occurrences of each word in a given sentence
 
+def word_count(sentence):
+    """function to count the occurence of each word in a sentence"""
+    words = sentence.split()
+    d = {}
+    for w in words:
+        if w in d:
+            d[w] +=1   # if word is aleardy in the dictionary increase the count by 1
+        else:
+            d[w]= 1    # if word is not in dictionary then add it to the dictionary count
+    return d
+
+print(word_count("This is a good day to  start something new."))
 
---
+++ W3Resource_Exercises/Strings/ex13.py
@@ -0,0 +1,5 @@
+#13. Write a Python script that takes input from the user and displays that input back in upper and lower cases.
+user_input = input('Please enter something')
+
+print(user_input.upper())
+print(user_input.lower())
