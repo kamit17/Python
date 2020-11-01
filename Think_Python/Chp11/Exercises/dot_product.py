fc12a32df8547d4efae2f3ffd506c647e72408bb
kamit17 <kamit17@outlook.com>
2020-10-31 20:50:26 -0500

    Think Python

--- Think_Python/Chp11/Exercises/dot_product.py
+++
@@ -1,2 +0,0 @@
-# Write a function dot_product(u, v) that takes two lists of numbers of the same length, and returns the sum of the products of the corresponding elements of each (the dot_product).
-
