65c4801052bfa6f8da71370b6be6e846906aada5
kamit17 <kamit17@outlook.com>
2020-11-11 03:11:05 -0600

    LPTHW

---
+++ LPTHW/ex16.py
@@ -0,0 +1,7 @@
+#close: Closes the file
+#read: Reads the contents of the file. You can assign the result to a variable
+#readline:Reads just one line of a text file.
+#truncate:Empties the file.
+#write('stuf'):writes "stuff" to the file.
+#seek(0):Moves the read/write location to the beginning of the file.
+
