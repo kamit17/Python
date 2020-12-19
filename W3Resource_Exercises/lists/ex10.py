53050798212f1397c685f019921e612798533a36
kamit17 <kamit17@outlook.com>
2020-12-18 19:38:16 -0600

    W3Resource

--- W3Resource_Exercises/lists/ex10.py
+++ W3Resource_Exercises/lists/ex10.py
@@ -14,5 +14,16 @@ def string_k(n,str):
     #iterate the loop till every substring
     for i in text:
         
-        #if length of current 
-    
+        #if length of current sub string is greater than n then
+        if len(i) > n:
+            
+            #append this substring in string list
+            string.append(i)
+            
+    return string
+
+
+#Driver Program
+n = 3
+str="Stairway to heaven"
+print(string_k(n,str))   
---
+++ W3Resource_Exercises/lists/ex15.py
@@ -0,0 +1,11 @@
+# Write a Python program to shuffle and print a specified list.
+
+#this can be done using the shuffle module
+
+from random import shuffle
+
+list1 = ['alpha','beta','theta','gamma','epsilon']
+
+shuffle(list1)
+
+print(list1)
