78fee094686ffc0dfa8d366c6eef2d7879c9caea
kamit17 <kamit17@outlook.com>
2021-02-01 22:39:47 -0600

    W3Resource

---
+++ W3Resource_Exercises/lists/ex22.py
@@ -0,0 +1,5 @@
+#Write a Python program to find the index of an item in a specified list.
+
+chars=['h','e','l','l','o']
+print(chars.index('l'))
+
---
+++ W3Resource_Exercises/lists/ex23.py
@@ -0,0 +1,8 @@
+#Write a Python program to flatten a shallow list.
+
+lst = [[10, 20, 30, 40], [50, 60, 70, 80], [90, 100, 110, 120]]
+flatlist = []
+for sublist in lst:
+   for item in sublist:
+      flatlist.append(item)
+print (flatlist)
