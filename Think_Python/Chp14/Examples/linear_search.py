02db0a11a6773896e861e7efbc0563cfb287da34
kamit17 <kamit17@outlook.com>
2021-09-08 12:43:00 -0500

    Think Python

---
+++ Think_Python/Chp14/Examples/linear_search.py
@@ -0,0 +1,18 @@
+from test import test
+
+friends = ["Joe","Zoe","Brad","Angelina","Zuki","Thandi","Paris"]
+
+
+
+def search_linear(xs,target):
+    """Find and return the index of target in sequence xs"""
+    for (i,v) in enumerate(xs):
+        if v == target:
+            return i
+    return -1
+
+
+test(search_linear(friends,"Zoe") == 1)
+test(search_linear(friends, "Joe") == 0)
+# test(search_linear(friends, "Paris") == 6)
+# test(search_linear(friends, "Bill") == -1)
