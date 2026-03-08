a9e4faf988ed0cbce24e925b02d0d7c4f0217288
Amit Kumar <kamit17@outlook.com>
2026-03-07 21:06:27 -0600

    MOOC_2026

---
+++ MOOC_2026/Part3/powers_of_two.py
@@ -0,0 +1,11 @@
+'''
+Please write a program which asks the user to type in an upper limit. The program then prints out numbers so that each subsequent number is the previous one doubled, starting from the number 1. That is, the program prints out powers of two in order.
+
+The execution of the program finishes when the next number to be printed would be greater than the limit set by the user. No numbers greater than the limit should be printed.
+'''
+upper_limit = int(input("Upper limit: "))
+i = 1
+while i <= upper_limit:
+    print(i)
+    i = i * 2
+    
