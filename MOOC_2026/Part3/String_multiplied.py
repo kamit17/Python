43c4b2bd031336e66081fc83e57a481dadd7766f
Amit Kumar <kamit17@outlook.com>
2026-03-09 22:48:26 -0500

    MOOC_2026

---
+++ MOOC_2026/Part3/String_multiplied.py
@@ -0,0 +1,9 @@
+'''
+Please write a program which asks the user for a string and an amount. The program then prints out the string as many times as specified by the amount. The printout should all be on one line, with no extra spaces or symbols added.
+'''
+
+
+input_string = input("Please type in a string: ")
+number = int(input("Please type in a number: "))
+
+print(input_string*number)
