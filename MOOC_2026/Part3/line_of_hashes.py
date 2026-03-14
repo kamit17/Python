2ca98a9fc035d9de706bd19765a953c3ae29a3d9
Amit Kumar <kamit17@outlook.com>
2026-03-13 22:29:38 -0500

    MOOC_2026

---
+++ MOOC_2026/Part3/line_of_hashes.py
@@ -0,0 +1,6 @@
+'''
+Please write a program which prints out a line of hash characters, the width of which is chosen by the user.
+'''
+width = int(input("Enter the width of the string: "))
+
+print(width * '#')
