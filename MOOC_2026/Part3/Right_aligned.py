a114bfefa0c135c86f43328c60d09310d3afa58c
Amit Kumar <kamit17@outlook.com>
2026-03-14 12:04:45 -0500

    MOOC_2026

---
+++ MOOC_2026/Part3/Right_aligned.py
@@ -0,0 +1,9 @@
+'''
+Please write a program which asks the user for a string and then prints it out so that exactly 20 characters are displayed. If the input is shorter than 20 characters, the beginning of the line is filled in with * characters.
+
+You may assume the input string is at most 20 characters long.
+
+Please type in a string: python
+
+**************python
+''''
---
+++ MOOC_2026/Part3/underlining.py
@@ -0,0 +1,15 @@
+'''
+Please write a program which asks the user for strings using a loop. The program prints out each string underlined as shown in the examples below. The execution ends when the user inputs an empty string - that is, just presses Enter at the prompt.
+
+'''
+
+
+
+
+
+while True:
+    input_string = input("Please type in a string: ")
+    if input_string == '':
+        break
+    print(input_string)
+    print(len(input_string)*'-')
